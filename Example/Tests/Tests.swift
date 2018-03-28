f5b1898485b01dc00e047a652b96edbad69028f3
Tomas Friml <instantni.med@gmail.com>
2018-03-28 14:18:04 +1300

    Added some tests

--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -2,48 +2,36 @@
 
 import Quick
 import Nimble
-import RxSplitSlider
+import SplitSlider
 
 class TableOfContentsSpec: QuickSpec {
     override func spec() {
-        describe("these will fail") {
-
-            it("can do maths") {
-                expect(1) == 2
-            }
-
-            it("can read") {
-                expect("number") == "string"
-            }
-
-            it("will eventually fail") {
-                expect("time").toEventually( equal("done") )
+        describe("Slider") {
+            
+            it("Adjust min") {
+                let slider = SplitSlider()
+                slider.min = 1
+                slider.max = 10
+                slider.left.value = 1
+                
+                slider.min = 2
+                expect(slider.left.value) == 2
+                
+                slider.min = 11
+                expect(slider.left.min) == 10
             }
             
-            context("these will pass") {
-
-                it("can do maths") {
-                    expect(23) == 23
-                }
-
-                it("can read") {
-                    expect("🐮") == "🐮"
-                }
-
-                it("will eventually pass") {
-                    var time = "passing"
-
-                    DispatchQueue.main.async {
-                        time = "done"
-                    }
-
-                    waitUntil { done in
-                        Thread.sleep(forTimeInterval: 0.5)
-                        expect(time) == "done"
-
-                        done()
-                    }
-                }
+            it("Adjust max") {
+                let slider = SplitSlider()
+                slider.min = 1
+                slider.max = 10
+                slider.left.value = 10
+                
+                slider.max = 9
+                expect(slider.left.value) == 9
+                
+                slider.max = 0
+                expect(slider.left.min) == 1
             }
         }
     }
